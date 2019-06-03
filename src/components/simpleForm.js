37650eedbb1b643f98a781e53167413563b403bd
damengrandom <damon@airteam.com.au>
2019-06-03 10:13:26 +1000

    make form data reloable based on localstorage

---
+++ src/components/simpleForm.js
@@ -0,0 +1,70 @@
+import React from "react"
+import { Field, Formik, Form } from "formik"
+
+class SimpleForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: localStorage.getItem("username")
+        ? localStorage.getItem("username")
+        : "",
+      password: localStorage.getItem("password")
+        ? localStorage.getItem("password")
+        : "",
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Formik
+          onSubmit={values => {
+            console.log("submit values", values)
+          }}
+        >
+          {props => (
+            <Form style={{ display: "flex", flexDirection: "column" }}>
+              <label htmlFor="username">Username</label>
+              <Field
+                type="text"
+                id="username"
+                name="username"
+                onChange={event =>
+                  this.setState({ username: event.target.value })
+                }
+                onBlur={event => {
+                  localStorage.setItem("username", event.target.value)
+                }}
+                defaultvalue={
+                  localStorage.getItem("username") !== "" ||
+                  (localStorage.getItem("username") &&
+                    localStorage.getItem("username"))
+                }
+                value={this.state.username}
+              />
+              <label htmlFor="password">Password</label>
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                onChange={event => {
+                  console.log("current", event.target.value)
+                  this.setState({ password: event.target.value })
+                }}
+                onBlur={event => {
+                  localStorage.setItem("password", event.target.value)
+                }}
+                value={this.state.password}
+              />
+              <button type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
+        {JSON.stringify(this.state)}
+      </>
+    )
+  }
+}
+
+export default SimpleForm
